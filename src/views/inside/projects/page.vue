3147f1ac973cb137224e5b4b04bcf83b7dbb62da
Ольга Фоменко <olgafomenko@MacBook-Pro-Olga.local>
2020-01-04 20:26:29 +0300

    созданы некоторые страницы

--- src/views/inside/projects/page.vue
+++ src/views/inside/projects/page.vue
@@ -1,15 +1,44 @@
 <template>
-    <v-container
-            class="fill-height"
-            fluid
-    >
-        <v-row
-                align="center"
-                justify="center"
-        >
-            <v-col class="text-center">
-                <h1>{{title}}</h1>
-                <p>Просто просмотр</p>
+    <v-container fluid>
+        <h1>{{title}}</h1>
+        <v-row>
+            <v-col cols="12" xl="6" lg="6" md="12" sm="12" xs="12">
+                <v-card elevation="0" outlined>
+                    <v-card-title>Родительские страницы</v-card-title>
+                    <v-card-text>
+                        <p>Страница 1</p>
+                        <p>Страница 2</p>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+            <v-col cols="12" xl="6" lg="6" md="12" sm="12" xs="12">
+                <v-card elevation="0" outlined>
+                    <v-card-title>Дочерние страницы</v-card-title>
+                    <v-card-text>
+                        <p>Страница 45</p>
+                        <p>Страница 46</p>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-card elevation="0" outlined>
+                    <v-card-text>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, architecto commodi
+                            repellat reprehenderit repudiandae suscipit. Et, excepturi magni minus modi ratione ullam
+                            voluptates. Error eveniet ipsa quae quaerat rem sed.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, architecto commodi
+                            repellat reprehenderit repudiandae suscipit. Et, excepturi magni minus modi ratione ullam
+                            voluptates. Error eveniet ipsa quae quaerat rem sed.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, architecto commodi
+                            repellat reprehenderit repudiandae suscipit. Et, excepturi magni minus modi ratione ullam
+                            voluptates. Error eveniet ipsa quae quaerat rem sed.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, architecto commodi
+                            repellat reprehenderit repudiandae suscipit. Et, excepturi magni minus modi ratione ullam
+                            voluptates. Error eveniet ipsa quae quaerat rem sed.</p>
+                    </v-card-text>
+                </v-card>
             </v-col>
         </v-row>
     </v-container>
@@ -38,7 +67,7 @@
         computed:{
             ...mapGetters("menuStates", ["getData"]),
             title(){
-                return 'Page ' + this.$route.params.id;
+                return `Просмотр страницы "${this.$route.params.id}"`;
             }
         },
 
