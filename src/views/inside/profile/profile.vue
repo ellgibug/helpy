fc2fccddeec938dec7938700c83f14d98dda9369
Ольга Фоменко <olgafomenko@MacBook-Pro-Olga.local>
2020-01-08 22:49:59 +0300

    inside

--- src/views/inside/profile/profile.vue
+++ src/views/inside/profile/profile.vue
@@ -1,23 +1,47 @@
 <template>
     <v-container fluid>
-        <h1>Профиль</h1>
+        <h1>Профиль (администратор)</h1>
         <v-row>
             <v-col>
                 <v-card elevation="0" outlined>
-                    <v-card-title>Персанальные данные</v-card-title>
+                    <v-card-title>Персональные данные</v-card-title>
                     <v-card-text>
-                        id, ФИО, емейл, телефон
+                        <p>Ваш ID клиента: 1234567</p>
+                        <v-text-field
+                                outlined
+                                class="my-5"
+                                color="pink accent-2"
+                                label="ФИО"
+                                single-line
+                                hide-details
+                        ></v-text-field>
+                        <v-text-field
+                                outlined
+                                class="mb-5"
+                                color="pink accent-2"
+                                label="Email"
+                                single-line
+                                hide-details
+                        ></v-text-field>
+                        <v-text-field
+                                outlined
+                                color="pink accent-2"
+                                label="Телефон"
+                                single-line
+                                hide-details
+                        ></v-text-field>
                     </v-card-text>
-                </v-card>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <v-card elevation="0" outlined>
-                    <v-card-title>Тип аккаунта</v-card-title>
-                    <v-card-text>
-                        админ, сотрудник
-                    </v-card-text>
+                    <v-card-actions class="text-center">
+                        <v-btn
+                                class="px-12 text-center"
+                                dark
+                                large
+                                rounded
+                                color="pink accent-2"
+                                depressed>
+                            Применить
+                        </v-btn>
+                    </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
@@ -26,8 +50,41 @@
                 <v-card elevation="0" outlined>
                     <v-card-title>Безопасность</v-card-title>
                     <v-card-text>
-                        пароль
+                        <v-text-field
+                                outlined
+                                class="my-5"
+                                color="pink accent-2"
+                                label="Текущий пароль"
+                                single-line
+                                hide-details
+                        ></v-text-field>
+                        <v-text-field
+                                outlined
+                                class="mb-5"
+                                color="pink accent-2"
+                                label="Новый пароль"
+                                single-line
+                                hide-details
+                        ></v-text-field>
+                        <v-text-field
+                                outlined
+                                color="pink accent-2"
+                                label="Подтвердить пароль"
+                                single-line
+                                hide-details
+                        ></v-text-field>
                     </v-card-text>
+                    <v-card-actions class="text-center">
+                        <v-btn
+                                class="px-12 text-center"
+                                dark
+                                large
+                                rounded
+                                color="pink accent-2"
+                                depressed>
+                            Применить
+                        </v-btn>
+                    </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
